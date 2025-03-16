3593f29fb2899004f18972c5745ea3c8391cac02
zaarirmoh <128648000+zaarirmoh@users.noreply.github.com>
2025-03-16 09:39:36 +0100

    student and teacher filters

---
+++ pfebackend/users/filters.py
@@ -0,0 +1,55 @@
+from django_filters import rest_framework as filters
+from django.db.models import Q
+from users.models import User
+
+class StudentFilter(filters.FilterSet):
+    enrollment_year = filters.NumberFilter(field_name="student__enrollment_year")
+    academic_program = filters.CharFilter(field_name="student__academic_program")
+    current_year = filters.NumberFilter(field_name="student__current_year")
+    academic_status = filters.CharFilter(field_name="student__academic_status")
+    speciality = filters.CharFilter(field_name="student__speciality")
+    has_team = filters.BooleanFilter(method='filter_has_team')
+    show_peers_only = filters.BooleanFilter(method='filter_peers_only')
+
+    class Meta:
+        model = User
+        fields = ['enrollment_year', 'academic_program', 'current_year', 
+                 'academic_status', 'speciality', 'has_team', 'show_peers_only']
+    
+    def filter_has_team(self, queryset, name, value):
+        if value:
+            return queryset.filter(teams__isnull=False)
+        else:
+            return queryset.filter(teams__isnull=True)
+        
+    def filter_peers_only(self, queryset, name, value):
+        # Only apply peer filtering if value is True
+        if not value:
+            return queryset
+            
+        request = self.request
+        if not request or not request.user or request.user.user_type != 'student':
+            return queryset
+            
+        # Get the student profile of the requesting user
+        try:
+            student = request.user.student
+            # Filter to only show peers (same year and program)
+            filtered_queryset = queryset.filter(
+                student__current_year=student.current_year,
+                student__academic_program=student.academic_program
+            )
+            # Exclude the requesting student
+            filtered_queryset = filtered_queryset.exclude(id=request.user.id)
+            return filtered_queryset
+        except AttributeError:
+            # If user doesn't have a student profile, return unfiltered
+            return queryset
+
+
+class TeacherFilter(filters.FilterSet):
+    department = filters.CharFilter(field_name="teacher__department")
+    
+    class Meta:
+        model = User
+        fields = ['department']
--- pfebackend/users/views.py
+++ pfebackend/users/views.py
@@ -2,73 +2,56 @@ from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from django.contrib.auth import get_user_model
-from users.models import Student
 from users.serializers.user import CustomUserSerializer
 from common.pagination import StaticPagination
-from django.db.models import Q
-
+from .filters import StudentFilter, TeacherFilter
 
 User = get_user_model()
 
-class StudentListView(generics.ListAPIView):
+class BaseUserListView(generics.ListAPIView):
+    """
+    Base view for user listing with common configurations
+    """
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = StaticPagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    
+    def get_queryset(self):
+        """
+        Abstract method to be implemented by subclasses
+        """
+        raise NotImplementedError("Subclasses must implement get_queryset")
+    
+    def get_filterset_context(self):
+        """
+        Pass the request to the filterset for user-context aware filtering
+        """
+        context = super().get_filterset_context()
+        context['request'] = self.request
+        return context
+
+
+class StudentListView(BaseUserListView):
     """
     API endpoint to retrieve all students.
     Supports filtering, searching, and ordering.
     """
-    serializer_class = CustomUserSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = StaticPagination
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = StudentFilter
     search_fields = ['first_name', 'last_name', 'email', 'username', 'student__matricule']
     ordering_fields = ['last_name', 'first_name', 'student__enrollment_year', 'student__current_year']
     
     def get_queryset(self):
-        queryset = User.objects.filter(user_type='student').select_related('student')
-        
-        filters = Q()
-        
-        filter_mapping = {
-            'enrollment_year': 'student__enrollment_year',
-            'academic_program': 'student__academic_program',
-            'current_year': 'student__current_year',
-            'academic_status': 'student__academic_status',
-            'speciality': 'student__speciality',
-        }
-        
-        # Dynamically build the Q object
-        for param, field in filter_mapping.items():
-            value = self.request.query_params.get(param)
-            if value:
-                filters &= Q(**{field: value})
-        
-        # Apply the filters to the queryset
-        queryset = queryset.filter(filters)
-        
-        has_no_team = self.request.query_params.get('has_no_team')
-        if has_no_team and has_no_team.lower() in ['true', '1', 'yes']:
-            queryset = queryset.exclude(teams__isnull=False)
-
-            
-        return queryset
+        return User.objects.filter(user_type='student').select_related('student')
     
-class TeacherListView(generics.ListAPIView):
+class TeacherListView(BaseUserListView):
     """
     API endpoint to retrieve all teachers.
     Supports filtering, searching, and ordering.
     """
-    serializer_class = CustomUserSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = StaticPagination
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = TeacherFilter
     search_fields = ['first_name', 'last_name', 'email', 'username']
     ordering_fields = ['last_name', 'first_name', 'email']
     
     def get_queryset(self):
-        queryset = User.objects.filter(user_type='teacher').select_related('teacher')
-        
-        # Filter by department if provided
-        department = self.request.query_params.get('department')
-        if department:
-            queryset = queryset.filter(teacher__department=department)
-            
-        return queryset
+        return User.objects.filter(user_type='teacher').select_related('teacher')
