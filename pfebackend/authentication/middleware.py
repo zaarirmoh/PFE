31b87d668c17172675f33146da4a0c4846a12264
Wbench12 <elbench4@gmail.com>
2025-03-19 07:47:08 +0100

    modify JWTAUTMIDDLEWARE in chat to include auth in token instead of in header

--- pfebackend/authentication/middleware.py
+++ pfebackend/authentication/middleware.py
@@ -6,18 +6,14 @@ from asgiref.sync import sync_to_async
 
 class JWTAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
-        # Extract JWT token from headers
-        headers = dict(scope["headers"])
-        auth_header = headers.get(b"authorization", b"").decode("utf-8")
+        query_string = scope.get("query_string", b"").decode("utf-8")
+        token = dict(x.split("=") for x in query_string.split("&")).get("token", "")
 
-        if auth_header.startswith("Bearer "):
-            token = auth_header.split(" ")[1]
+        if token:
             try:
-                # Validate JWT token
-                jwt_authentication = JWTAuthentication()
-                validated_token = jwt_authentication.get_validated_token(token)
-                user = await sync_to_async(jwt_authentication.get_user)(validated_token)
-                scope["user"] = user
+                jwt_auth = JWTAuthentication()
+                validated_token = jwt_auth.get_validated_token(token)
+                scope["user"] = await sync_to_async(jwt_auth.get_user)(validated_token)
             except (InvalidToken, AuthenticationFailed):
                 scope["user"] = AnonymousUser()
         else:
