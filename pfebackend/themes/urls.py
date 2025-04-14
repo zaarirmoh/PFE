71c57b68e5287c8a9076bfd08c398b7242a0d1cf
Wbench12 <elbench4@gmail.com>
2025-04-14 19:28:13 +0100

    tt

--- pfebackend/themes/urls.py
+++ pfebackend/themes/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.theme_creation_viewsets import ThemeViewSet
-from .views.theme_assignment_viewsets import ThemeChoiceViewSet, ThemeAssignmentViewSet
+# from .views.theme_assignment_viewsets import ThemeChoiceViewSet, ThemeAssignmentViewSet
 
 router = DefaultRouter()
 router.register(r'themes', ThemeViewSet, basename="theme")
-router.register(r'choices', ThemeChoiceViewSet, basename="theme-choice")
-router.register(r'assignments', ThemeAssignmentViewSet, basename="theme-assignment")
+# router.register(r'choices', ThemeChoiceViewSet, basename="theme-choice")
+# router.register(r'assignments', ThemeAssignmentViewSet, basename="theme-assignment")
 urlpatterns = [
     path('', include(router.urls)),
 ]
