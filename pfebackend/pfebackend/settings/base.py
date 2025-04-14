f915001d0602d23d9bec98d54ff239240195b258
Wbench12 <elbench4@gmail.com>
2025-04-15 00:33:08 +0100

    FIX: correct redis host for celery broker for both docker and dev envs

--- pfebackend/pfebackend/settings/base.py
+++ pfebackend/pfebackend/settings/base.py
@@ -193,11 +193,19 @@ SWAGGER_SETTINGS = {
 }
 
 
-# CELERY SETTINGS
-CELERY_BROKER_URL = 'redis://:my_password@localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://:my_password@localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+
+#for myenv
+# CELERY_BROKER_URL = 'redis://:my_password@localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://:my_password@localhost:6379/0'
+#ta3 docker
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/1"
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 
 
