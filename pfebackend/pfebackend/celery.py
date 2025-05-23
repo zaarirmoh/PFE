abd0234f6e33c75dacf66bb6d45ebaf512d79e20
Wbench12 <elbench4@gmail.com>
2025-05-23 11:20:51 +0100

    FIX : celery task scheduling

--- pfebackend/pfebackend/celery.py
+++ pfebackend/pfebackend/celery.py
@@ -1,13 +1,17 @@
 import os
 from celery import Celery
-# from django.conf import settings 
+from django.conf import settings
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pfebackend.settings.dev')
 
 app = Celery('pfebackend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
 
+# Fix for broker_connection_retry warning
+app.conf.broker_connection_retry_on_startup = True
 
+# Explicitly discover tasks from all installed apps
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task(bind=True)
 def debug_task(self):
---
+++ pfebackend/supervision/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'supervision.apps.SupervisionConfig'
